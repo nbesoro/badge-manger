9b7aa2d4d5d1ee419fc30746eb55bcdd94dfc238
Mamoutou Doumbia <mamoutoudoumbia@Mamoutous-MacBook-Pro.local>
2024-06-22 16:26:33 +0000

    update

--- badgemanager/badgemanager/settings.py
+++ badgemanager/badgemanager/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-u)ur@2nzk7mslhrmus@9bl2@fe&@%#r_bsska1ozz@cr&6!trk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
